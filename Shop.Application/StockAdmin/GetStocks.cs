c939e17e7f34616253985d373ad1137e5aafeffc
idupcek <ivan.dupcek@gmail.com>
2020-09-15 06:44:19 +1200

    crud for stock

---
+++ Shop.Application/StockAdmin/GetStocks.cs
@@ -0,0 +1,42 @@
+﻿using Shop.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Application.StockAdmin
+{
+    public class GetStocks
+    {
+        private readonly ApplicationDbContext _ctx;
+
+        public GetStocks(ApplicationDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public IEnumerable<StockViewModel> Do(int productId)
+        {
+            var stock = _ctx.Stock
+                .Where(s => s.ProductId == productId)
+                .Select(s => new StockViewModel
+                { 
+                    Id = s.Id, 
+                    Description = s.Description, 
+                    Qty = s.Qty
+                })
+                .ToList();
+
+            return stock;
+        }
+
+        public class StockViewModel
+        {
+            public int Id { get; set; }
+            public int ProductId { get; set; }
+            public string Description { get; set; }
+            public string Qty { get; set; }
+        }
+    }
+}
