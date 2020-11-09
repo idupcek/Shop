0eeb825f52d59d872eec06b3560972e5ac07b11a
idupcek <ivan.dupcek@gmail.com>
2020-11-10 08:16:01 +1300

    setting up stock management, vue

---
+++ Shop.Application/StockAdmin/GetStock.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shop.Database;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Shop.Application.StockAdmin
+{
+    public class GetStock
+    {
+        private readonly ApplicationDbContext _ctx;
+
+        public GetStock(ApplicationDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public IEnumerable<ProductViewModel> Do()
+        {
+            var stock = _ctx.Products
+                .Include(s => s.Stock)
+                .Select(p => new ProductViewModel
+                { 
+                    Id = p.Id,
+                    Description = p.Description,
+                    Stock = p.Stock.Select(s => new StockViewModel
+                    {
+                        Id = s.Id,
+                        Description = s.Description,
+                        Qty = s.Qty
+                    })
+                })
+                .ToList();
+
+            return stock;
+        }
+
+        public class StockViewModel
+        {
+            public int Id { get; set; }
+            public int ProductId { get; set; }
+            public string Description { get; set; }
+            public string Qty { get; set; }
+        }
+
+        public class ProductViewModel
+        {
+            public int Id { get; set; }
+            public string Description { get; set; }
+            public IEnumerable<StockViewModel> Stock { get; set; }
+        }
+    }
+}
---
+++ Shop.UI/wwwroot/js/admin/stock.js
@@ -0,0 +1,52 @@
+﻿var app = new Vue({
+    el: '#app',
+    data: {
+        products: [],
+        selectedProduct: null,
+        newStock: {
+            productId: 0,
+            description: "Size",
+            qty: 10
+        }
+    },
+    mounted() {
+        this.getStock()
+    },
+    methods: {
+        getStock() {
+            this.loading = true;
+            axios.get('/Admin/stocks')
+                .then(res => {
+                    console.log(res)
+                    this.products = res.data;
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+                .then(() => {
+                    this.loading = false;
+                })
+        },
+        updateStock() {
+
+        },
+        addStock() {
+            this.loading = true;
+            axios.post('/Admin/stocks', this.newStock)
+                .then(res => {
+                    console.log(res)
+                    this.products.stock.push(res.data);
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+                .then(() => {
+                    this.loading = false;
+                })
+        },
+        selectProduct(product) {
+            this.selectedProduct = product;
+            this.newStock.productId = product.Id
+        }
+    }
+})
